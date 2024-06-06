0c05dc69799e6017bcc65694216b81509de6e1ac
enocquet <emmanuelnocquet8@gmail.com>
2024-06-06 15:36:27 +0200

    changed providers defs

--- main.tf
+++ main.tf
@@ -1,21 +1,5 @@
 locals {
-  tls_secret_name        = var.tls_certificate_type != "none" ? var.tls_secret_name : ""
-}
-
-provider "kubernetes" {
-  config_path = var.config_path
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = var.config_path
-  }
-}
-
-provider "kubectl" {
-  config_path = var.config_path
-
-  load_config_file = true
+  tls_secret_name = var.tls_certificate_type != "none" ? var.tls_secret_name : ""
 }
 
 resource "random_password" "prom_admin_password" {
