055e834673b5dc6d45e476142ad2a058685f851b
enocquet <emmanuelnocquet8@gmail.com>
2024-07-10 10:04:01 +0200

    Add config params to nginx

--- create-keycloak/main.tf
+++ create-keycloak/main.tf
@@ -1,26 +1,26 @@
 locals {
-  namespace          = "keycloak"
+  namespace = "keycloak"
 
   pvc_name = "${local.namespace}-pvc"
   pv_name  = "${local.namespace}-pv"
 
   keycloak_values = {
-    "NAMESPACE" = local.namespace
-    "SECRET" = "keycloak-config"
-    "ADMIN_USER" = var.keycloak_admin_user
-    "ADMIN_PASSWORD_SECRET_KEY" = "keycloak_admin_password"
-    "INGRESS_HOSTNAME" = var.keycloak_ingress_hostname
-    "POSTGRES_USER" = var.keycloak_postgres_user
-    "POSTGRES_PASSWORD_SECRET_KEY" = "keycloak_postgres_password"
+    "NAMESPACE"                          = local.namespace
+    "SECRET"                             = "keycloak-config"
+    "ADMIN_USER"                         = var.keycloak_admin_user
+    "ADMIN_PASSWORD_SECRET_KEY"          = "keycloak_admin_password"
+    "INGRESS_HOSTNAME"                   = var.keycloak_ingress_hostname
+    "POSTGRES_USER"                      = var.keycloak_postgres_user
+    "POSTGRES_PASSWORD_SECRET_KEY"       = "keycloak_postgres_password"
     "POSTGRES_ADMIN_PASSWORD_SECRET_KEY" = "keycloak_postgres_admin_password"
-    "PVC_NAME" = var.is_bare_metal ? local.pvc_name : ""
+    "PVC_NAME"                           = var.is_bare_metal ? local.pvc_name : ""
   }
 }
 
 resource "kubernetes_namespace" "keycloak_namespace" {
-    metadata {
-        name = local.namespace
-    }
+  metadata {
+    name = local.namespace
+  }
 }
 
 resource "kubernetes_persistent_volume_v1" "postgresql-pv" {
@@ -63,7 +63,7 @@ resource "kubernetes_persistent_volume_v1" "postgresql-pv" {
 }
 
 resource "kubernetes_persistent_volume_claim_v1" "postgres-pvc" {
-  count = var.is_bare_metal ? 1 : 0 
+  count = var.is_bare_metal ? 1 : 0
   metadata {
     name      = local.pvc_name
     namespace = local.namespace
@@ -81,31 +81,31 @@ resource "kubernetes_persistent_volume_claim_v1" "postgres-pvc" {
 }
 
 resource "helm_release" "keycloak-postgresql" {
-    name       = "keycloak-postgresql"
-    repository = var.postgres_helm_repo
-    chart      = var.postgres_helm_chart
-    version    = var.postgres_helm_chart_version
-    namespace  = local.namespace
+  name       = "keycloak-postgresql"
+  repository = var.postgres_helm_repo
+  chart      = var.postgres_helm_chart
+  version    = var.postgres_helm_chart_version
+  namespace  = local.namespace
 
-    values = [
-        templatefile("${path.module}/values-postgresql.yaml", local.keycloak_values)
-    ]
+  values = [
+    templatefile("${path.module}/values-postgresql.yaml", local.keycloak_values)
+  ]
 
-    depends_on = [kubernetes_namespace.keycloak_namespace]
+  depends_on = [kubernetes_namespace.keycloak_namespace]
 }
 
 resource "helm_release" "keycloak" {
-    name       = "keycloak"
-    repository = var.keycloak_helm_repo
-    chart      = var.keycloak_helm_chart
-    version    = var.keycloak_helm_chart_version
-    namespace  = local.namespace
-    
-    values = [
-        templatefile("${path.module}/values-keycloak.yaml", local.keycloak_values)
-    ]
+  name       = "keycloak"
+  repository = var.keycloak_helm_repo
+  chart      = var.keycloak_helm_chart
+  version    = var.keycloak_helm_chart_version
+  namespace  = local.namespace
 
-    depends_on = [kubernetes_namespace.keycloak_namespace, helm_release.keycloak-postgresql]
+  values = [
+    templatefile("${path.module}/values-keycloak.yaml", local.keycloak_values)
+  ]
+
+  depends_on = [kubernetes_namespace.keycloak_namespace, helm_release.keycloak-postgresql]
 }
 
 resource "random_password" "keycloak_admin_password" {
@@ -125,7 +125,7 @@ resource "random_password" "keycloak_postgresql_admin_password" {
 
 resource "kubernetes_secret" "keycloak_config" {
   metadata {
-    name = "keycloak-config"
+    name      = "keycloak-config"
     namespace = local.namespace
     labels = {
       "app" = "keycloak"
@@ -133,11 +133,11 @@ resource "kubernetes_secret" "keycloak_config" {
   }
 
   data = {
-    keycloak_admin_user     = local.keycloak_values.ADMIN_USER
-    keycloak_admin_password = random_password.keycloak_admin_password.result
-    keycloak_postgres_user  = local.keycloak_values.POSTGRES_USER
-    keycloak_postgres_password  = random_password.keycloak_postgresql_password.result
-    keycloak_postgres_admin_password  = random_password.keycloak_postgresql_admin_password.result
+    keycloak_admin_user              = local.keycloak_values.ADMIN_USER
+    keycloak_admin_password          = random_password.keycloak_admin_password.result
+    keycloak_postgres_user           = local.keycloak_values.POSTGRES_USER
+    keycloak_postgres_password       = random_password.keycloak_postgresql_password.result
+    keycloak_postgres_admin_password = random_password.keycloak_postgresql_admin_password.result
   }
 
   type = "Opaque"
--- create-keycloak/variables.tf
+++ create-keycloak/variables.tf
@@ -1,10 +1,10 @@
 variable "keycloak_admin_user" {
-  type = string
+  type    = string
   default = "admin-cosmo"
 }
 
 variable "keycloak_postgres_user" {
-  type = string
+  type    = string
   default = "keycloak_postgres_user"
 }
 
@@ -17,31 +17,31 @@ variable "is_bare_metal" {
 }
 
 variable "keycloak_helm_repo" {
-  type = string
+  type    = string
   default = "https://charts.bitnami.com/bitnami"
 }
 
 variable "keycloak_helm_chart" {
-  type = string
+  type    = string
   default = "keycloak"
 }
 
 variable "keycloak_helm_chart_version" {
-  type = string
+  type    = string
   default = "21.3.1"
 }
 
 variable "postgres_helm_repo" {
-  type = string
+  type    = string
   default = "https://charts.bitnami.com/bitnami"
 }
 
 variable "postgres_helm_chart" {
-  type = string
+  type    = string
   default = "postgresql"
 }
 
 variable "postgres_helm_chart_version" {
-  type = string
+  type    = string
   default = "15.5.1"
 }
