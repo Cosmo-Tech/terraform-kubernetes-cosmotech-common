f7ae2ffd83c9583a7466027135148e0a03f18e27
nibaldo <nibaldo.donoso@cosmotech.com>
2024-08-22 10:56:02 +0200

    add argocd vault and sops deployment [skip ci]

--- modules.tf
+++ modules.tf
@@ -69,3 +69,64 @@ module "keycloak" {
   is_bare_metal             = var.is_bare_metal
   provisioner               = var.provisioner
 }
+
+module "create_argocd" {
+  source = "./create_argocd"
+
+  count = var.create_argocd ? 1 : 0
+
+  namespace                      = var.argocd_namespace
+  helm_repo_url                  = var.argocd_helm_repo_url
+  helm_chart                     = var.argocd_helm_chart
+  helm_chart_version             = var.argocd_helm_chart_version
+  helm_release_name              = var.argocd_helm_release_name
+  replicas                       = var.argocd_replicas
+  create_ingress                 = var.argocd_create_ingress
+  argocd_project                 = var.argocd_project
+  argocd_repositories            = var.argocd_repositories
+  argocd_repository_username     = var.argocd_repository_username
+  argocd_repository_access_token = var.argocd_repository_access_token
+  argocd_dns_name                = var.api_dns_name
+  argocd_setup_job_image_version = var.argocd_setup_job_image_version
+
+  depends_on = [module.cert-manager]
+}
+
+module "create_vault" {
+  source = "./create_vault"
+
+  count = var.create_vault ? 1 : 0
+
+  namespace             = var.vault_namespace
+  helm_repo_url         = var.vault_helm_repo_url
+  helm_chart            = var.vault_helm_chart
+  helm_chart_version    = var.vault_helm_chart_version
+  helm_release_name     = var.vault_helm_release_name
+  vault_replicas        = var.vault_replicas
+  vault_secret_name     = var.vault_secret_name
+  vault_ingress_enabled = var.vault_ingress_enabled
+  vault_dns_name        = var.api_dns_name
+
+  depends_on = [module.cert-manager]
+}
+
+module "create_vault_secrets_operator" {
+  source = "./create-vault-secrets-operator"
+
+  count = var.create_vault_secrets_operator ? 1 : 0
+
+  namespace          = var.vault_secrets_operator_namespace
+  helm_repo_url      = var.vault_secrets_operator_helm_repo_url
+  helm_chart         = var.vault_secrets_operator_helm_chart
+  helm_chart_version = var.vault_secrets_operator_helm_chart_version
+  helm_release_name  = var.vault_secrets_operator_helm_release_name
+  vault_address      = var.vault_secrets_operator_vault_address
+  allowed_namespaces = var.vault_secrets_operator_allowed_namespaces
+  replicas           = var.vault_secrets_operator_replicas
+  vault_namespace    = var.vault_namespace
+  tenant_id          = var.tenant_id
+  cluster_name       = var.cluster_name
+  organization       = var.customer_name
+
+  depends_on = [module.create_vault]
+}
