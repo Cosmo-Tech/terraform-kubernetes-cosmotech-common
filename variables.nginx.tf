1f34aacc8ada9b0eb57eb91a024a65938cf12531
nibaldo <nibaldo.donoso@cosmotech.com>
2024-09-02 11:08:56 +0200

    add variables nginx [skip ci]

--- variables.nginx.tf
+++ variables.nginx.tf
@@ -1,8 +1,40 @@
+variable "nginx_helm_repo_url" {
+  type    = string
+  default = "https://kubernetes.github.io/ingress-nginx"
+}
+
+variable "nginx_helm_release_name" {
+  type    = string
+  default = "ingress-nginx"
+}
+
 variable "ingress_nginx_version" {
   type    = string
   default = "4.2.5"
 }
 
+variable "nginx_namespace" {
+  type    = string
+  default = "ingress-nginx"
+}
+
+variable "monitoring_namespace" {
+  type    = string
+  default = "cosmotech-monitoring"
+}
+
 variable "publicip_address" {
   type = string
+}
+
+variable "tls_secret_name" {
+  type = string
+}
+
+variable "publicip_resource_group" {
+  type = string
+}
+
+variable "is_bare_metal" {
+  type = bool
 }
