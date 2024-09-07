7f750e78724ded504c7596a1a3c0569431ac7d3c
nibaldo <nibaldo.donoso@cosmotech.com>
2024-09-07 22:52:44 +0200

    update providers kubernetes kubectl and helm

--- create-vault-secrets-operator/providers.tf
+++ create-vault-secrets-operator/providers.tf
@@ -2,18 +2,11 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.20.0"
-    }
-    kubectl = {
-      source  = "alekc/kubectl"
-      version = "2.0.4"
+      version = "2.32.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.9.0"
+      version = "2.15.0"
     }
   }
-  required_version = ">= 1.3.9"
 }
-
-
