73d5666b9b421483d25f081bdfbd1e9201ce9a35
Joel LOURENCO <joel.lourenco.ext@cosmotech.com>
2024-10-04 12:09:45 +0200

    Fix conditions for vars start_aks_minutes & start_aks_hours [skip-ci]

--- create_vault/variables.tf
+++ create_vault/variables.tf
@@ -41,7 +41,7 @@ variable "auto_restart_deploy" {
 variable "start_aks_minutes" {
   type = number
   validation {
-    condition = can(regex(var.start_aks_minutes, range(0,60)))
+    condition = var.start_aks_minutes >= 0 && var.start_aks_minutes < 60
     error_message = "Minutes must be between 0 and 59"
   }
 }
@@ -49,7 +49,7 @@ variable "start_aks_minutes" {
 variable "start_aks_hours" {
   type = number
   validation {
-    condition = can(regex(var.start_aks_minutes, range(0,24)))
+    condition = var.start_aks_hours >= 0 && var.start_aks_hours < 24
     error_message = "Hours must be between 0 and 23"
   }
 }
