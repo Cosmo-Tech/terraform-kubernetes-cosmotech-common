01c656c470d39e263c9e5ccf3a388c8a2d2741e3
Barny <barnabas.lukacs.ext@cosmotech.com>
2024-11-18 17:33:48 +0200

    Helm chart tekton pipelines

---
+++ create-tektonh/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "2.0.4"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.15.0"
+    }
+  }
+}
