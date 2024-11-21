f68bd37dd6cbb4e2bc63e6ad1336f82a81f7a9b0
Barny <barnabas.lukacs.ext@cosmotech.com>
2024-11-21 13:49:51 +0200

    deploy triggers

--- modules.tf
+++ modules.tf
@@ -182,13 +182,13 @@ module "create_tekton" {
   dashboard_helm_chart              = var.tekton_dashboard_helm_chart
   dashboard_helm_chart_version      = var.tekton_dashboard_helm_chart_version
   dashboard_helm_release_name       = var.tekton_dashboard_helm_release_name
-  triggers_helm_repo_url           = var.tekton_triggers_helm_repo_url
-  triggers_helm_chart              = var.tekton_triggers_helm_chart
-  triggers_helm_chart_version      = var.tekton_triggers_helm_chart_version
-  triggers_helm_release_name       = var.tekton_triggers_helm_release_name
-  interceptors_helm_repo_url           = var.tekton_interceptors_helm_repo_url
-  interceptors_helm_chart              = var.tekton_interceptors_helm_chart
-  interceptors_helm_chart_version      = var.tekton_interceptors_helm_chart_version
-  interceptors_helm_release_name       = var.tekton_interceptors_helm_release_name
+  triggers_helm_repo_url            = var.tekton_triggers_helm_repo_url
+  triggers_helm_chart               = var.tekton_triggers_helm_chart
+  triggers_helm_chart_version       = var.tekton_triggers_helm_chart_version
+  triggers_helm_release_name        = var.tekton_triggers_helm_release_name
+  interceptors_helm_repo_url        = var.tekton_interceptors_helm_repo_url
+  interceptors_helm_chart           = var.tekton_interceptors_helm_chart
+  interceptors_helm_chart_version   = var.tekton_interceptors_helm_chart_version
+  interceptors_helm_release_name    = var.tekton_interceptors_helm_release_name
 
 }
