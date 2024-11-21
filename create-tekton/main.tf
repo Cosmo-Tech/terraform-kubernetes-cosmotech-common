db025a9b67a524e2d90644fc86072c4d861c9644
Barny <barnabas.lukacs.ext@cosmotech.com>
2024-11-21 13:42:47 +0200

    Tekton triggers and interceptors

--- create-tekton/main.tf
+++ create-tekton/main.tf
@@ -1,9 +1,16 @@
-terraform {
-  required_providers {
-    kubectl = {
-      source  = "alekc/kubectl"
-      version = "2.0.4"
-    }
+locals {
+  values_tekton = {
+    "NAMESPACE"             = var.namespace
+  }
+  instance_name = var.helm_release_name
+  dashboard_name = var.dashboard_helm_release_name
+  triggers_name = var.triggers_helm_release_name
+  interceptors_name = var.interceptors_helm_release_name
+}
+
+resource "kubernetes_namespace" "tekton_namespace" {
+  metadata {
+    name = var.namespace
   }
 }
 
