81cbff14e90ef3f0fb9d99fd75780ca94dfeb685
nibaldo <nibaldo.donoso@cosmotech.com>
2024-12-11 15:20:57 +0100

    add backup velero deployment

--- modules.tf
+++ modules.tf
@@ -175,8 +175,8 @@ module "deploy_velero" {
   velero_bucket_name                   = var.velero_bucket_name
   velero_storage_account_name          = var.velero_storage_account_name
   velero_storage_account_resource_name = var.velero_storage_account_resource_name
-  velero_bakcup_client_id              = var.velero_bakcup_client_id
-  velero_bakcup_client_secret          = var.velero_bakcup_client_secret
+  velero_backup_client_id              = var.velero_backup_client_id
+  velero_backup_client_secret          = var.velero_backup_client_secret
   velero_backup_resource_group_cluster = var.velero_backup_resource_group_cluster
   velero_storage_account_access_key    = var.velero_storage_account_access_key
   velero_release_name                  = var.velero_release_name
@@ -186,5 +186,4 @@ module "deploy_velero" {
   velero_namespace                     = var.velero_namespace
   velero_azure_subcription_id          = var.velero_azure_subcription_id
   velero_azure_tenant_id               = var.velero_azure_tenant_id
-
 }
