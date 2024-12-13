3433a85183a1925cf201dd6172a5055ccd992fdf
nibaldo <nibaldo.donoso@cosmotech.com>
2024-12-13 21:00:06 +0100

    add rbac

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   tls_secret_name        = var.tls_certificate_type == "let_s_encrypt" ? "letsencrypt-prod" : "custom-tls-secret"
-  kube_config            = data.azurerm_kubernetes_cluster.current.kube_config
+  kube_config            = data.azurerm_kubernetes_cluster.current.kube_admin_config
   host                   = local.kube_config.0.host
   client_certificate     = base64decode(local.kube_config.0.client_certificate)
   client_key             = base64decode(local.kube_config.0.client_key)
@@ -12,27 +12,3 @@ data "azurerm_kubernetes_cluster" "current" {
   resource_group_name = var.kubernetes_resource_group
 }
 
-provider "kubernetes" {
-  host                   = local.host
-  client_certificate     = local.client_certificate
-  client_key             = local.client_key
-  cluster_ca_certificate = local.cluster_ca_certificate
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = local.host
-    client_certificate     = local.client_certificate
-    client_key             = local.client_key
-    cluster_ca_certificate = local.cluster_ca_certificate
-  }
-}
-
-provider "kubectl" {
-  host                   = local.host
-  client_certificate     = local.client_certificate
-  client_key             = local.client_key
-  cluster_ca_certificate = local.cluster_ca_certificate
-
-  load_config_file = false
-}
