ff76671077364c5d32df6300b3572ccc061245fc
nibaldo <nibaldo.donoso@cosmotech.com>
2025-02-28 16:01:20 +0100

    remove providers settings

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.17.0"
+      version = "2.12.1"
     }
     kubectl = {
       source  = "alekc/kubectl"
@@ -18,45 +18,4 @@ terraform {
     }
   }
   required_version = ">= 1.3.9"
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-  resource_provider_registrations = "none"
-  subscription_id                 = var.subscription_id
-  client_id                       = var.client_id
-  client_secret                   = var.client_secret
-  tenant_id                       = var.tenant_id
-}
-
-provider "azuread" {
-  tenant_id     = var.tenant_id
-  client_id     = var.client_id
-  client_secret = var.client_secret
-}
-
-provider "kubernetes" {
-  host                   = local.host
-  client_certificate     = local.client_certificate
-  client_key             = local.client_key
-  cluster_ca_certificate = local.cluster_ca_certificate
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = local.host
-    client_certificate     = local.client_certificate
-    client_key             = local.client_key
-    cluster_ca_certificate = local.cluster_ca_certificate
-  }
-}
-
-provider "kubectl" {
-  host                   = local.host
-  client_certificate     = local.client_certificate
-  client_key             = local.client_key
-  cluster_ca_certificate = local.cluster_ca_certificate
-
-  load_config_file = false
 }
