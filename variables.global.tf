0b59addf3d6b8befb3fbd36d21310f524c0f61a8
nibaldo <nibaldo.donoso@cosmotech.com>
2025-03-03 10:35:14 +0100

    test new config 2

--- variables.global.tf
+++ variables.global.tf
@@ -9,3 +9,16 @@ variable "monitoring_namespace" {
 variable "cluster_name" {
   type = string
 }
+
+variable "host" {
+  type = string
+}
+variable "client_certificate" {
+  type = string
+}
+variable "client_key" {
+  type = string
+}
+variable "cluster_ca_certificate" {
+  type = string
+}
