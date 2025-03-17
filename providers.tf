76a7189a4878ee7c9944c13e5e9a3b40ff6c2e47
nibaldo <nibaldo.donoso@cosmotech.com>
2025-03-17 17:23:09 +0100

    add prov conf

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.17.0"
+      version = "2.15.0"
     }
     kubectl = {
       source  = "alekc/kubectl"
@@ -18,4 +18,29 @@ terraform {
     }
   }
   required_version = ">= 1.3.9"
+}
+
+provider "kubernetes" {
+  host                   = var.host
+  client_certificate     = var.client_certificate
+  client_key             = var.client_key
+  cluster_ca_certificate = var.cluster_ca_certificate
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = var.host
+    client_certificate     = var.client_certificate
+    client_key             = var.client_key
+    cluster_ca_certificate = var.cluster_ca_certificate
+  }
+}
+
+provider "kubectl" {
+  host                   = var.host
+  client_certificate     = var.client_certificate
+  client_key             = var.client_key
+  cluster_ca_certificate = var.cluster_ca_certificate
+
+  load_config_file = false
 }
